55752c196a8111a149a13d8daa503704d920f145
salma sbaiti <salmasbaiti1@gmail.com>
2023-04-10 23:35:54 +0000

    auth required for new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\HomeController;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,11 +36,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/home' , [HomeController::class, 'index'] );
+    Route::resource('/clients' , 'App\Http\Controllers\ClientController');
+    Route::resource('/dossiers' , 'App\Http\Controllers\DossierController');
 });
 
-Route::resource('/clients' , 'App\Http\Controllers\ClientController');
-Route::resource('/dossiers' , 'App\Http\Controllers\DossierController');
-Route::get('/test' , function(){
-    return Inertia::render('Home') ;
-}) ;
+// Route::get('/test' , function(){
+//     return Inertia::render('Home') ;
+// }) ;
+
 require __DIR__.'/auth.php';
