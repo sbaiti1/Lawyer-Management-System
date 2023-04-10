842dc7970b3962377448e2f1ad409ab26007ab35
salma sbaiti <salmasbaiti1@gmail.com>
2023-04-10 02:39:04 +0000

    display data from props

--- resources/js/Pages/Client/Table.tsx
+++ resources/js/Pages/Client/Table.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { PageProps } from '@/types';
 
-const data = [
+/*const data = [
   { id: 1, name: 'علي', age: 25, city: 'جدة' },
   { id: 2, name: 'محمد', age: 30, city: 'الرياض' },
   { id: 3, name: 'فاطمة', age: 27, city: 'الدمام' },
   { id: 4, name: 'نورة', age: 22, city: 'مكة المكرمة' },
-];
+];*/
 
-const Table = () => {
+
+interface TableProps {
+  data: {
+    id : number ;
+    nom: string;
+    prenom: string;
+    email: string;
+    CIN: string;
+    phone: string;
+  }[];
+}
+
+const Table : React.FC<TableProps> = ({data}) => {
   return (
     <div className="w-full overflow-x-auto">
       <table className="w-full table-auto">
@@ -23,9 +36,9 @@ const Table = () => {
           {data.map((item) => (
             <tr key={item.id} className="border-b bg-gray-100">
               <td className="px-4 py-2 text-center">{item.id}</td>
-              <td className="px-4 py-2 text-center">{item.name}</td>
-              <td className="px-4 py-2 text-center">{item.age}</td>
-              <td className="px-4 py-2 text-center">{item.city}</td>
+              <td className="px-4 py-2 text-center">{item.nom}</td>
+              <td className="px-4 py-2 text-center">{item.prenom}</td>
+              <td className="px-4 py-2 text-center">{item.phone}</td>
             </tr>
           ))}
         </tbody>
