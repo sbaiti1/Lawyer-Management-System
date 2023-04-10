a8fa76742a3da267610699c5616035d4358c57f5
salma sbaiti <salmasbaiti1@gmail.com>
2023-04-10 23:36:42 +0000

    tiny updates

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -6,23 +6,25 @@ import Calendar from "react-calendar";
 import Navbar from "@/Components/Navbar";
 import Sidebar from "@/Components/Sidebar";
 import Table from "./Client/Table";
-const CustomCalendar = () => {
-  const [date, setDate] = useState(new Date());
+import { PageProps } from '@/types';
 
-  
+interface TableProps extends PageProps {
+  total : number
+  totalD : number
+  data: {
+    id : number ;
+    nom: string;
+    prenom: string;
+    email: string;
+    CIN: string;
+    phone: string;
+  }[];
+}
 
-  return (
-    <div className="mx-auto max-w-2xl">
-       
-  
-    </div>
-  );
-};
 
 
 
-
-const Home: React.FC = () => {
+const Home: React.FC<TableProps> = ( props : TableProps ) => {
   return (
     <div dir="rtl" className="h-screen flex  bg-slate-100">
       <div className="w-fit flex-shrink-0">
@@ -33,33 +35,33 @@ const Home: React.FC = () => {
         <div className="p-4">
         <div className="mt-5 w-2/3">
   <div className="flex flex-wrap gap-8 items-stretch">
-    <div className="w-full md:w-1/2 lg:w-1/4 mb-4 mx-2">
+    <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2">
       <div className="flex flex-col bg-sky-400 rounded-lg h-full shadow-lg">
         <div className="flex-grow p-4">
-          <h2 className="text-white text-lg font-bold mb-2">Card 1</h2>
-          <p className="text-white text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h2 className="text-white text-lg font-bold mb-2">إجمالي الموكلين</h2>
+          <p className="text-white text-sm"> {props.total} موكل</p>
         </div>
       </div>
     </div>
-    <div className="w-full md:w-1/2 lg:w-1/4 mb-4 mx-2" >
+    <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2" >
       <div className="flex flex-col bg-blue-500 rounded-lg h-full">
         <div className="flex-grow p-4">
-          <h2 className="text-white text-lg font-bold mb-2">Card 2</h2>
-          <p className="text-white text-sm">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <h2 className="text-white text-lg font-bold mb-2">إجمالي الملفات</h2>
+          <p className="text-white text-sm"> {props.totalD} ملف</p>
         </div>
       </div>
     </div>
-    <div className="w-full md:w-1/2 lg:w-1/4 mb-4 mx-2">
+    <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2">
       <div className="flex flex-col bg-emerald-500 rounded-lg h-full">
         <div className="flex-grow p-4">
-          <h2 className="text-white text-lg font-bold mb-2">Card 3</h2>
-          <p className="text-white text-sm">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <h2 className="text-white text-lg font-bold mb-2">إجمالي جلسات اليوم</h2>
+          <p className="text-white text-sm"> 6 جلسات </p>
         </div>
       </div>
     </div>
     
   </div>
-  <Table/>
+ <Table data={props.data} /> 
 </div>
 
         </div>
