f13fba2b1c9c157a6cdce3d3e0d865bd303fb20a
salma sbaiti <salmasbaiti1@gmail.com>
2023-04-12 17:36:16 +0000

    updated cards

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -7,6 +7,9 @@ import Navbar from "@/Components/Navbar";
 import Sidebar from "@/Components/Sidebar";
 import Table from "./Client/Table";
 import { PageProps } from '@/types';
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+import PeopleIcon from '@mui/icons-material/People';
+import GavelIcon from '@mui/icons-material/Gavel';
 
 interface TableProps extends PageProps {
   total : number
@@ -26,47 +29,45 @@ interface TableProps extends PageProps {
 
 const Home: React.FC<TableProps> = ( props : TableProps ) => {
   return (
-    <div dir="rtl" className="h-screen flex  bg-slate-100">
-      {/* <div className="w-fit flex-shrink-0">
-        <Sidebar />
-      </div> */}
-      <div className="w-full">
+    <div dir="rtl" className="h-screen bg-slate-50">
+     
         <Navbar />
-        <div className="p-4">
-        <div className="mt-5 w-2/3">
-  <div className="flex flex-wrap gap-8 items-stretch">
-    <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2">
-      <div className="flex flex-col bg-sky-400 rounded-lg h-full shadow-lg">
-        <div className="flex-grow p-4">
-          <h2 className="text-white text-lg font-bold mb-2">إجمالي الموكلين</h2>
-          <p className="text-white text-sm"> {props.total} موكل</p>
-        </div>
-      </div>
-    </div>
-    <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2" >
-      <div className="flex flex-col bg-blue-500 rounded-lg h-full">
-        <div className="flex-grow p-4">
-          <h2 className="text-white text-lg font-bold mb-2">إجمالي الملفات</h2>
-          <p className="text-white text-sm"> {props.totalD} ملف</p>
-        </div>
-      </div>
-    </div>
-    <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2">
-      <div className="flex flex-col bg-emerald-500 rounded-lg h-full">
-        <div className="flex-grow p-4">
-          <h2 className="text-white text-lg font-bold mb-2">إجمالي جلسات اليوم</h2>
-          <p className="text-white text-sm"> 6 جلسات </p>
-        </div>
-      </div>
-    </div>
+        <div className="p-4 flex  py-8">
+        <div className="w-2/3">
+            <div className="cards">
+            <div className="flex flex-wrap gap-8 items-stretch">
+            <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2">
+              <div className="flex flex-col bg-white rounded-md h-full shadow-md cursor-pointer hover:scale-105 transform transition duration-300">
+                <div className="flex-grow p-4">
+                  <h2 className="text-amber-300 text-lg font-bold mb-2"> <PeopleIcon style={{color : '#fcd34d'}}/> إجمالي الموكلين</h2>
+                  <p className="text-gray-500 text-sm"> {props.total} موكل</p>
+                </div>
+              </div>
+            </div>
+          <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2" >
+            <div className="flex flex-col bg-white shadow-md cursor-pointer hover:scale-105 transform transition duration-300 rounded-lg h-full">
+              <div className="flex-grow p-4">
+                <h2 className="text-blue-500 text-lg font-bold mb-2"> <FolderCopyIcon style={{color : '#3b82f6'}}/> إجمالي الملفات </h2>
+                <p className="text-gray-500 text-sm"> {props.totalD} ملف</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-fit md:w-1/2 lg:w-1/4 mb-4 mx-2">
+            <div className="flex flex-col bg-white shadow-md cursor-pointer hover:scale-105 transform transition duration-300 rounded-lg h-full">
+              <div className="flex-grow p-4">
+                <h2 className="text-emerald-500 text-lg font-bold mb-2"><GavelIcon style={{color : '#10b981'}}/> جلسات اليوم</h2>
+                <p className="text-gray-500 text-sm"> 6 جلسات </p>
+              </div>
+            </div>
+          </div>
     
-  </div>
- <Table data={props.data} /> 
-</div>
-
+        </div>
+                  </div>
+ 
+        </div>
+          <div className="bg-red-500 h-10"></div>
         </div>
   </div>
-</div>
 );
 };
 
