84c30b01426c561fae1baa2e4e9a446712eb1586
salma sbaiti <salmasbaiti1@gmail.com>
2023-04-13 16:20:43 +0000

    added avatar

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Drawer from "./Drawer";
+import Avatar from '@mui/material/Avatar';
+
 const Navbar: React.FC = () => {
     return (
       <nav className=" w-full drop-shadow p-2 bg-white flex justify-between items-center">
@@ -11,12 +13,12 @@ const Navbar: React.FC = () => {
         
         {/* Profile */}
         <div className="flex items-center">
-          <img
-            src="https://via.placeholder.com/50"
-            alt="Profile"
-            className="w-10 h-10 rounded-full mx-2"
-          />
-          <span className="text-white text-lg font-medium">اسم المستخدم</span>
+        <Avatar
+        alt="Remy Sharp"
+        src="https://images.unsplash.com/photo-1573497019236-17f8177b81e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        sx={{ width: 45, height: 45 }}
+      />
+          <span className="text-gray-500 text-md px-3 font-medium">اسم المستخدم</span>
         </div>
       </nav>
     );
