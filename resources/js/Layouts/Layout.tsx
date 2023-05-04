0ff258bd6875ad394e40528e5ee63c610e2dee06
salma sbaiti <salmasbaiti1@gmail.com>
2023-05-04 13:58:12 +0100

    role optional

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -4,14 +4,15 @@ import Navbar from "@/Components/Navbar";
 
 interface Props {
     children: React.ReactNode;
-    role : string 
+    role? : string 
   }
 
 const Layout:React.FC<Props> = (props : Props)=>{
     return(
         <div dir="rtl" className="min-h-screen bg-slate-50">
      
-        <Navbar role={props.role} />
+      
+        <Navbar role={props.role || "اسم المستخدم"} />
         <div className="p-4 flex  py-8">
         
                 {props.children}
