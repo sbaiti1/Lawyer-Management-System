989795518ad2eafec46eab1ac4a4fc03548001e1
salma sbaiti <salmasbaiti1@gmail.com>
2023-05-08 00:27:31 +0100

    updated table

--- resources/js/Pages/Client/Table.tsx
+++ resources/js/Pages/Client/Table.tsx
@@ -1,33 +1,9 @@
+import React from 'react';
 import { PageProps } from '@/types';
-
-import * as React from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-
-const columns: GridColDef[] = [
-  { field: 'id', headerName: '#', width: 70 },
-  { field: 'nom', headerName: 'الاسم', width: 130 },
-  { field: 'prenom', headerName: ' النسب', width: 130  },
-  {
-    field: 'CIN',
-    headerName: 'CIN',
-    type: 'number',
-    width: 120,
-  },
-  {
-    field: 'phone',
-    headerName: 'الهاتف',
-    type: 'number',
-    width: 200,
-  },
-  { field: 'email', headerName: ' البريد الإلكتروني', width: 250 },
-
-  
-];
-
-
+import { Link } from '@inertiajs/react';
 interface TableProps {
   data: {
-    id : number ;
+    id: number;
     nom: string;
     prenom: string;
     email: string;
@@ -36,17 +12,71 @@ interface TableProps {
   }[];
 }
 
-const Table : React.FC<TableProps> = ({data}) => {
-  const rows = data ;
+const Table: React.FC<TableProps> = ({ data }) => {
   return (
-    <div style={{ height: 400, width: '100%' , background : 'white' }}>
-    <DataGrid 
-      rows={rows}
-      columns={columns}
-      checkboxSelection
-      
-    />
-  </div>
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              #
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              الاسم
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              النسب
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              CIN
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              الهاتف
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              البريد الإلكتروني
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td className="px-6 py-4 whitespace-nowrap ">{item.id}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-center">{item.nom}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-center">{item.prenom}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-center">{item.CIN}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-center">{item.phone}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-center">{item.email}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-center">
+                <Link href={`/clients/${item.id}`}>
+                  <span className="text-indigo-600 hover:text-indigo-900">
+                  المزيد
+                  </span>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
